139fee81bc438af942031d058f202a66ac1a71d9
Hashrocket <jasshultz@gmail.com>
2017-08-30 15:59:51 -0600

    new page extension

---
+++ background.js
@@ -0,0 +1,24 @@
+/*
+Open a new tab, and load "my-page.html" into it.
+*/
+function openMyPage() {
+  console.log('injecting');
+   browser.tabs.create({
+     "url": '/my-page.html'
+   });
+}
+
+
+/*
+Add openMyPage() as a listener to clicks on the browser action.
+*/
+browser.browserAction.onClicked.addListener(openMyPage);
+
+function handleCreated(tab) {
+	console.log('tabi id', tab.id);
+	document.body.innerHTML = '';
+	document.documentElement.innerHTML = '';
+}
+
+browser.tabs.onCreated.addListener(handleCreated);
+ 
